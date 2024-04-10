c103cdf94a0e4073fb1418525c597c24ce5c5590
tolerevo <tolerevo@hotmail.com>
2024-04-10 17:00:01 -0600

    shuri the cat

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+// Add react-router-dom imports
+
+// create router with JSX Route elements
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+      <Route path=":type" element={<HomePage />} />
+      <Route path="/:type/:id" element={ <PetDetailsPage /> } />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    // replace below with a Router Provider
+    <RouterProvider router={appRouter} />
+  );
+}
+
+export default App;
