f9dfcddef692f34244c4b96e7ee2f3c443cfc2f2
tolerevo <tolerevo@hotmail.com>
2024-04-10 18:32:42 -0600

    mend

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const appRouter = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path=":type" element={<HomePage />} />
       <Route path="/:type/:id" element={<PetDetailsPage />} />
+      <Route path="search" element={<SearchPage />} />
     </Route>
   )
 );
